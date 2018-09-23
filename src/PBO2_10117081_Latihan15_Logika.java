57a7e508db88a76a21641525dd312d32f749ddd6
GeoSyah <43498342+GeoSyah@users.noreply.github.com>
2018-09-23 14:25:57 +0700

    Add files via upload

---
+++ src/PBO2_10117081_Latihan15_Logika.java
@@ -0,0 +1,28 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Geo Syah Alkautsar
+ * 10117081 IF-2
+ * Menampilkan operasi bilangan menggunakan operator logika
+ */
+public class PBO2_10117081_Latihan15_Logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " + (a && b));
+        System.out.println("a || b = " + (a || b));
+        System.out.println("!a = " + !a);
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
